0880f79e8c6fa384bf0ad7145386bc280c4b3cb1
Wing <w.gayongan@gmail.com>
2024-07-13 16:56:12 +0000

    fix: logo href

--- client/src/components/page/layout/header/Header.tsx
+++ client/src/components/page/layout/header/Header.tsx
@@ -8,12 +8,7 @@ import Navigation from "./Navigation";
 export default function Header() {
   return (
     <header className="w-screen flex border-b shadow-sm py-1 px-10 items-center ">
-      <Link
-        href="/create"
-        as="/create"
-        prefetch
-        className="flex space-x-5 items-center"
-      >
+      <Link href="/" as="/" prefetch className="flex space-x-5 items-center">
         <HanapBHLogo className="h-6 w-auto" />
         <h1 className="text-xl font-bold italic">Hanap BH</h1>
       </Link>
