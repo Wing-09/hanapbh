892fa4cbd403b810c70bee70d5b4b2cdfbab7a99
Wilfreno Gayongan <wilfrenogayongan@gmail.com>
2024-07-28 21:42:16 +0800

    testing if docker/metadata-action@v5 works

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-  branches: [{ name: "test", prerelease: true }, { name: "main" }],
+  branches: [{ name: "github-action", prerelease: true }, { name: "main" }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
