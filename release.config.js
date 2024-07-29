70e492f8dec691255ca342285f77d35aa0f64b12
Wilfreno Gayongan <wilfrenogayongan@gmail.com>
2024-07-29 13:56:42 +0800

    fix(Github Action): set release.config to only run for the main branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-  branches: [{ name: "test", prerelease: true }, { name: "main" }],
+  branches: [{ name: "main" }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
