944eb5e94df7d49b5843d474b6789fb19c3816b3
Wilfreno <w.gayongan@gmail.com>
2024-08-19 22:07:25 +0800

    feat: create Occupant model

--- server/src/database/model/Occupant.ts
+++ server/src/database/model/Occupant.ts
@@ -1,9 +1,47 @@
-import { Types } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 
 export type OccupantType = {
   room: Types.ObjectId;
   user: Types.ObjectId;
-  status: "occupying" | "left";
+  status: "OCCUPYING" | "LEFT";
   joined: Date;
-  left: Date;
+  left: Date | "PRESENT";
 };
+
+const occupantSchema = new Schema<OccupantType>({
+  room: {
+    type: Schema.Types.ObjectId,
+    ref: "Room",
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  status: {
+    type: String,
+    enum: ["OCCUPYING", "LEFT"],
+    required: true,
+  },
+  joined: {
+    type: Date,
+    default: Date.now,
+  },
+  left: {
+    type: Date || String,
+    enum: ["PRESENT"],
+    default: "PRESENT",
+  },
+});
+
+occupantSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+const Occupant = model("Occupant", occupantSchema);
+
+export default Occupant;
