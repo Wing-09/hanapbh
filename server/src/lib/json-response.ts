09d0fc0ec31936a6360647f8684953976809fa2f
Wilfreno <w.gayongan@gmail.com>
2024-08-23 04:11:19 +0800

    feat: create read property api route

--- server/src/lib/json-response.ts
+++ server/src/lib/json-response.ts
@@ -8,11 +8,33 @@ type ResponseStatus =
   | "BAD_REQUEST"
   | "CREATED";
 
+/**
+ *
+ *  Constructs a JSON for a uniform format of response.
+ *
+ *
+ * @param status - string form of the status code
+ * @param message - response message
+ * @param data - the result of the requested data
+ *
+ * @returns {object}
+ *
+ * @requires fastifyReply as `reply`
+ * @example
+ *
+ *  return reply.code(200).send(JSONResponse("OK", "request successful", data))
+ *
+ *  // the message has a default value that corresponds the 500 code error
+ *  return reply.code(500).send(JSONResponse("EXTERNAL_SERVER_ERROR"))
+ *
+ *
+ */
+
 export default function JSONResponse<T>(
   status: ResponseStatus,
   message: string = "oops! something went wrong",
   data: T | null = null
-) {
+): object {
   return {
     status,
     message,
